4f5216b920ed8b1bedd9141aa01e9db44a9eeaf6
ali <85955355+Alidl81@users.noreply.github.com>
2024-10-15 10:51:45 +0330

    Update blinker.ino

--- blinker.ino
+++ blinker.ino
@@ -1,29 +1,24 @@
-int led1=5;
-int led2=6;
-int led3=7;
+int led=5;
+// int led1=6;
+// int led2=7;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
+  pinMode(led,OUTPUT);
+  
+  // pinMode(led1,OUTPUT);
+  // pinMode(led2,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    digitalWrite(led1,HIGH);
-    delay(100);
-    digitalWrite(led2,HIGH);
-    delay(100);
-    digitalWrite(led3,HIGH);
-    digitalWrite(led1,LOW);
-    delay(100);
-    digitalWrite(led2,LOW);
-    delay(100);
-    digitalWrite(led3,LOW);
-    delay(1000);
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+
+  
   // digitalWrite(led1,HIGH);
-  //     digitalWrite(led1,HIGH);
   // digitalWrite(led2,HIGH);
   // delay(3000);
   // digitalWrite(led1,LOW);
