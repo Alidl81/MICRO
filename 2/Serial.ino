7542daffd8f845618ddc89ae851a2a8cd77b1432
ali <85955355+Alidl81@users.noreply.github.com>
2024-10-15 14:53:47 +0330

    Update Serial.ino

--- 2/Serial.ino
+++ 2/Serial.ino
@@ -1,14 +1,12 @@
-int led1=5;
-int led2=6;
-int led3=7;
-int led4=8;
+int led1=4;
+
+// int led2=5;
+// int led2=6;
+// int led2=7;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(led1,OUTPUT);
-  pinMode(led2,OUTPUT);
-  pinMode(led3,OUTPUT);
-  pinMode(led4,OUTPUT);
 
 }
 
@@ -32,27 +30,14 @@ if(Serial.available()>0){
 //   String control =Serial.readStringUntil('\n');
 //   if(control=="H" || control=="h" ||  control=="on" || control=="1"){
 //     digitalWrite(led1, HIGH);
-//     digitalWrite(led2, HIGH);
-//     digitalWrite(led3, HIGH);
-//     digitalWrite(led4, HIGH);
 //   }else if(control=="L" || control=="l" ||  control=="off" || control=="0"){
 //     digitalWrite(led1, LOW);
-//     digitalWrite(led2, LOW);
-//     digitalWrite(led3, LOW);
-//     digitalWrite(led4, LOW);
 //   }else{
 //   for(int i=0;i<=20;i++){
 //       digitalWrite(led1, HIGH);
-//       digitalWrite(led2, HIGH);
-//       digitalWrite(led3, HIGH);
-//       digitalWrite(led4, HIGH);
 //       delay(25);
 //       digitalWrite(led1, LOW);
-//       digitalWrite(led2, LOW);
-//       digitalWrite(led3, LOW);
-//       digitalWrite(led4, LOW);
 //       delay(25);
-
 //     }
 //   }
 // }
@@ -66,11 +51,13 @@ if(Serial.available()>0){
 //       digitalWrite(led2, HIGH);
 //   }else if(control=="saleh"){
 //       digitalWrite(led3, HIGH);
+//   }else if(control=="mohamad"){
+//       digitalWrite(led4, HIGH);
 //   }else if(control=="off"){
 //       digitalWrite(led1, LOW);
 //       digitalWrite(led2, LOW);
 //       digitalWrite(led3, LOW);
-//   }else{
+//   }else {
 //       digitalWrite(led1, HIGH);
 //       digitalWrite(led2, HIGH);
 //       digitalWrite(led3, HIGH);
